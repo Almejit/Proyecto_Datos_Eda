555c8b8f20e3115d74c9114a0295c48477120d88
Almejit <tonicm2005@gmail.com>
2025-11-14 20:23:01 +0100

    14/11/2025

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from src.carga_datos import cargar_datos
 from src.exploracion import explorar_datos
+from src.DefiniciónProblemas.DiseñoGráficos import visualizar_exploracion
 
 def main():
 
     df = cargar_datos()
     explorar_datos(df)
+
+    visualizar_exploracion(df, guardar=True)
 
 
 if __name__ == "__main__":
