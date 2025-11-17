36943647a1f74bfa0f9ad36cf9923fc68a4b65f9
santiago25gotor <santiago25goam@gmail.com>
2025-11-17 19:29:58 +0100

    Update limpieza_datos
    Arreglado

--- src/limpieza_datos.py
+++ src/limpieza_datos.py
@@ -5,19 +5,19 @@ import seaborn as sns
 
 
 def detectar_outliers(df):
+#
+    numerical_cols = df.select_dtypes(include=['number']).columns
 
-numerical_cols = df.select_dtypes(include=['number']).columns
+    num_plots = len(numerical_cols)
+    fig, axes = plt.subplots(nrows=num_plots, ncols=1, figsize=(10, 5 * num_plots))
 
-num_plots = len(numerical_cols)
-fig, axes = plt.subplots(nrows=num_plots, ncols=1, figsize=(10, 5 * num_plots))
+    if num_plots == 1:
+        axes = [axes]
 
-if num_plots == 1:
-    axes = [axes]
+    for i, col in enumerate(numerical_cols):
+        sns.boxplot(y=df[col], ax=axes[i])
+        axes[i].set_title(f'Box plot de {col}')
+        axes[i].set_ylabel(col)
 
-for i, col in enumerate(numerical_cols):
-    sns.boxplot(y=df[col], ax=axes[i])
-    axes[i].set_title(f'Box plot de {col}')
-    axes[i].set_ylabel(col)
-
-plt.tight_layout()
-plt.show()
+    plt.tight_layout()
+    plt.show()
