e3d1fc96ddaa4353bf6969dd141a1b33548c0e2b
santiago25gotor <santiago25goam@gmail.com>
2025-11-19 20:13:47 +0100

    Recién estructurado
    La limpieza de datos ya está completa. No falta nada.

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from src.carga_datos import cargar_datos
 from src.exploracion import explorar_datos
-from src.DefiniciónProblemas.DiseñoGráficos import visualizar_exploracion
+#from src.DefiniciónProblemas.DiseñoGráficos import visualizar_exploracion
+
+from src.limpieza_datos import detectar_outliers, eliminar_outliers, tratar_valores_nulos
 
 def main():
 
     df = cargar_datos()
+
     explorar_datos(df)
 
-    visualizar_exploracion(df, guardar=True)
+    detectar_outliers(df)
+
+    eliminar_outliers(df, columnas=['AveragePrice', 'Total Volume'])
+
+    tratar_valores_nulos(df)
+
+    #visualizar_exploracion(df, guardar=True)
 
 
 if __name__ == "__main__":
